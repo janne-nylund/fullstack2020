186bd4dcb9b4b606336437143842461c294882a5
janne-nylund <janne.nylund@centria.fi>
2020-09-19 14:01:26 +0300

    Do exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics = (props) => {
-  return (
+  
+    if (props.good + props.neutral + props.bad === 0){
+      return(
+        <div>
+          <h1>statistics</h1>
+          <p>No feedback given</p>
+        </div>
+      )
+    }
+    return (
     <div>
       <h1>statistics</h1>
       <p>good {props.good}</p>
