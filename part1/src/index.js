9f52073fe892ff1870ac7e985edc9f15b820397c
janne-nylund <janne.nylund@centria.fi>
2020-09-23 10:29:58 +0300

    Update examples part1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({ onClick, text }) => (
+const Button = ({ onClick, text }) => {
+  return (
   <button onClick={onClick}>
     {text}
   </button>
-)
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
 const App = (props) => {
   const [left, setLeft] = useState(0)
