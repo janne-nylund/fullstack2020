bf2fba8cf7a6a43f01465e0d1c5bb2185f246fc9
janne-nylund <janne.nylund@centria.fi>
2020-09-28 12:18:58 +0300

    Do exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
     // array.some -> if atleast one of the elements match
     if (persons.some(person => person.name === newName)) {
-      window.alert(newName + ' is already added to the phonebook')
+      window.alert(`${newName} is already added to the phonebook`)
     } else {
       setPersons(persons.concat(nameObject))
       setNewName('')
