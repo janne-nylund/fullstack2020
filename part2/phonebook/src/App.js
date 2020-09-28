22ace7f94f4d18c2e326a24cfae2c7053aa43ab6
janne-nylund <janne.nylund@centria.fi>
2020-09-28 13:27:44 +0300

    Do exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -35,12 +35,12 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    //console.log(event.target.value)
+    // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    //console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
