26fb1897b365e8dc5ce7294a613686a54945c15f
janne-nylund <janne.nylund@centria.fi>
2020-10-29 15:21:23 +0200

    Do exercise 4.2

--- part4/index.js
+++ part4/index.js
@@ -1,65 +1,10 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-// require('dotenv').config()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
-logger.info('connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.use(middleware.unknownEndpoint)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
