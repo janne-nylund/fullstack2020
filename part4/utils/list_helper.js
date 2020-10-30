ecd72c058894b4f7d9242cee2fc061471f481b18
janne-nylund <janne.nylund@centria.fi>
2020-10-30 12:54:16 +0200

    Do exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -12,7 +12,32 @@ const totalLikes = array => {
     : array.reduce(reducer, 0)
 }
 
+const favoriteBlog = array => {
+  const reducer = (max, item) => {
+    return Math.max(max, item.likes)
+  }
+
+  const mostLikes = array.reduce(reducer, 0)
+  const blog = array.find(item => item.likes === mostLikes)
+  /* console.log(array.length === 0
+    ? { author: 'No blogs found' }
+    : {
+      'title': blog.title,
+      'author': blog.author,
+      'likes': blog.likes
+    }
+  ) */
+  return array.length === 0
+    ? { author: 'No blogs found' }
+    : {
+      'title': blog.title,
+      'author': blog.author,
+      'likes': blog.likes
+    }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
