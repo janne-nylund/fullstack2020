c4addc83023758da295aca6687cacdf93659951a
janne-nylund <janne.nylund@centria.fi>
2020-11-18 18:39:40 +0200

    Do exercises 6.3-6.5

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch({
+      type: 'ADD_LIKE',
+      data: { id }
+    })
+  }
+  
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const getId = () => (100000 * Math.random()).toFixed(0)
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: {
+        content,
+        id: getId(),
+        votes: 0
+      }
+    })
+  }
+  
+  const sortByVotes = (a, b) => {
+    return b.votes - a.votes
+  }
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.sort(sortByVotes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form  onSubmit={addAnecdote}>
+        <div><input  name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
