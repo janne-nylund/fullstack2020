9eb8f619a16e5b16ca50cae5a65c9541c2943f8f
janne-nylund <janne.nylund@centria.fi>
2020-11-20 16:32:39 +0200

    Do exercise 7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -38,9 +38,6 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
- /*  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('') */
   const content = useField('text')
   const author = useField('text')
   const info = useField('text')
